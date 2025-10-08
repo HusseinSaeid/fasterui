bbc8b11f30022701db9911effe7a95854d233be8
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2025-10-08 19:31:39 +0300

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${raleway.variable} antialiased `}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
       </body>
